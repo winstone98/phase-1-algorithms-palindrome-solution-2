2ececd437af159c3e779eb8107accc626b713242
root <root@HomeLPT-001.localdomain>
2022-06-18 22:30:09 +0300

    Done

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
 */
 
 /*
